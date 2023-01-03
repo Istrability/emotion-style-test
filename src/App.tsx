e9d337de7e18c57ddd5a3463f19660fbb8a07cbd
croraf <vrafaeli@msn.com>
2023-01-03 16:46:09 +0100

    Add emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { jsx } from "@emotion/react";
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      css={{
+        backgroundColor: "hotpink",
+        "&:hover": {
+          color: "lightgreen",
+        },
+      }}
+    >
+      TEST
     </div>
   );
-}
+};
 
 export default App;
